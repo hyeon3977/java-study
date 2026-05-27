3644a58d67e578c556a27cb3c805449f19caece5
hyeon3977 <tsb07070@gmail.com>
2026-05-28 01:40:42 +0900

    feat: HashMap 기반 카테고리별 지출 통계 및 옛날 데이터 Null 방어 로직 구현

--- MyAccountManager.java
+++ MyAccountManager.java
@@ -4,8 +4,10 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -14,15 +16,23 @@ class Transaction implements Serializable {
 
     String date, title, type;
     int amount;
+    String category;
 
     public Transaction(String date, String title, String type, int amount) {
+        this(date, title, type, amount, "-");
+    }
+
+    public Transaction(String date, String title, String type, int amount, String category) {
         this.date = date;
         this.title = title;
         this.type = type;
         this.amount = amount;
+        this.category = category;
     }
 
     private String getPaddedCenter(String target, int width) {
+        if (target == null) target = "-";
+
         int currentWidth = 0;
         for (char c : target.toCharArray()) {
             if (c >= '\uAC00' && c <= '\uD7A3') currentWidth += 2;
@@ -47,10 +57,11 @@ class Transaction implements Serializable {
     @Override
     public String toString() {
         String pDate = getPaddedCenter(date, 10);
-        String pTitle = getPaddedCenter(title, 20);
-        String pType = getPaddedCenter(type, 12);
+        String pTitle = getPaddedCenter(title, 18);
+        String pType = getPaddedCenter(type, 10);
+        String pCategory = getPaddedCenter(category, 10);
 
-        return String.format("%s|%s|%s|%,12d원", pDate, pTitle, pType, amount);
+        return String.format("%s|%s|%s|%s|%,12d원", pDate, pTitle, pType, pCategory, amount);
     }
 }
 
@@ -66,6 +77,12 @@ public class MyAccountManager {
 
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
             history = (List<Transaction>) ois.readObject();
+
+            for (Transaction t : history) {
+                if (t.category == null) {
+                    t.category = "-";
+                }
+            }
 
             recalculateBalance();
 
@@ -92,12 +109,12 @@ public class MyAccountManager {
         System.out.println("입금 완료: " + title + " (" + amount + "원)");
     }
 
-    public void withdraw(String date, String title, int amount, boolean isFixed) {
+    public void withdraw(String date, String title, int amount, boolean isFixed, String category) {
         String type = isFixed ? "고정지출" : "유동지출";
 
         if (currentBalance >= amount) {
             currentBalance -= amount;
-            history.add(new Transaction(date, title, type, amount));
+            history.add(new Transaction(date, title, type, amount, category));
             System.out.println("지출 완료: " + title + " [" + type + "] (" + amount + "원)");
         } else {
             System.out.println("잔고 부족으로 '" + title + "' 결제 실패 (현재 잔액: " + currentBalance + "원)");
@@ -113,11 +130,11 @@ public class MyAccountManager {
                 }
             });
         }
-        System.out.println("\n==========================================================================");
-        System.out.println("   번호   |    날짜    |         항목         |     구분     |      금액      ");
-        System.out.println("--------------------------------------------------------------------------");
+        System.out.println("\n==================================================================================");
+        System.out.println("  번호   |   날짜   |       항목       |   구분   |  카테고리  |    금액      ");
+        System.out.println("----------------------------------------------------------------------------------");
         if (history.isEmpty()) {
-            System.out.println("                         기록된 내역이 없습니다.                        ");
+            System.out.println("                            기록된 내역이 없습니다.                           ");
         } else {
             for (int i = 0; i < history.size(); i++) {
                 System.out.printf(" [%2d ]  |%s%n", (i + 1), history.get(i));
@@ -130,8 +147,8 @@ public class MyAccountManager {
 
     public void showRangeReport(String startDate, String endDate) {
         System.out.println("\n======================= 기간별 조회 (" + startDate + " ~ " + endDate + ") =======================");
-        System.out.println("   번호   |    날짜    |         항목         |     구분     |      금액      ");
-        System.out.println("--------------------------------------------------------------------------");
+        System.out.println("   번호   |    날짜    |         항목         |     구분     |  카테고리  |      금액      ");
+        System.out.println("----------------------------------------------------------------------------------");
 
         Collections.sort(history, (t1,t2) -> t1.date.compareTo(t2.date));
 
@@ -155,15 +172,20 @@ public class MyAccountManager {
         int totalFixedWithdraw = 0;
         int totalVariavleWithdraw = 0;
 
+        Map<String, Integer> categoryMap = new HashMap();
+
         for (Transaction t : history) {
             if (t.type.equals("입금")) {
                 totalDeposit += t.amount;
-            } else if (t.type.equals("고정지출")) {
-                totalFixedWithdraw += t.amount;
+            } else {
+                if (t.type.equals("고정지출")) {
+                    totalFixedWithdraw += t.amount;
             } else if (t.type.equals("유동지출")) {
                 totalVariavleWithdraw += t.amount;
             }
+            categoryMap.put(t.category, categoryMap.getOrDefault(t.category, 0) + t.amount);
         }
+    }
 
         System.out.println("\n====================== 지출/수입 통계 ======================");
         System.out.printf("     총 입금액     : %,35d원%n",totalDeposit);
@@ -172,6 +194,16 @@ public class MyAccountManager {
         System.out.println("------------------------------------------------------------");
         System.out.printf("     총 지출 합계   : %,35d원%n",(totalFixedWithdraw + totalVariavleWithdraw));
         System.out.println("============================================================\n");
+        System.out.println(" [카테고리별 세부 지출 통계]");
+
+        if (categoryMap.isEmpty()) {
+            System.out.println("  - 가록된 지출 카테고리가 없습니다.");
+        } else {
+            for (Map.Entry<String, Integer> entry : categoryMap.entrySet()) {
+                System.out.printf("  • %-10s : %,33d원%n", "[" + entry.getKey() + "]", entry.getValue());
+            }
+            System.out.println("============================================================\n");
+        }
     }
 
     public void deleteTransaction(int index) {
@@ -180,12 +212,13 @@ public class MyAccountManager {
         recalculateBalance();
     }
 
-    public void updateTransaction(int index, String date, String title, int amount) {
+    public void updateTransaction(int index, String date, String title, int amount, String category) {
         if (index >= 0 && index < history.size()) {
             Transaction t = history.get(index);
             t.date = date;
             t.title = title;
             t.amount = amount;
+            t.category = category;
             recalculateBalance();
         }
     }
@@ -211,7 +244,8 @@ public class MyAccountManager {
         System.out.println("----------------------------------------------------------------------------");
         boolean found = false;
         for (Transaction t : history) {
-            if (t.title.contains(keyword)) {
+            String targetCat = t.category != null ? t.category : "-";
+            if (t.title.contains(keyword) || t.category.contains(keyword)) {
                 System.out.printf(" [ 결과 ]  |%s%n", t);
                 found = true;
             }
@@ -263,22 +297,68 @@ public class MyAccountManager {
                 case 1:
                     System.out.print("날짜(MMDD): ");
                     String dDate = formatDate(scanner.nextLine());
+                    if (!isValidDate(dDate)) {
+                        System.out.println("날짜 형식이 올바르지 않습니다. (예: 0520 -> 50-20)");
+                        break;
+                    }
+
                     System.out.print("항목: ");
                     String dTitle = scanner.nextLine();
                     System.out.print("금액: ");
-                    int dAmount = scanner.nextInt();
+                    int dAmount;
+                    try {
+                        dAmount = scanner.nextInt();
+                        scanner.nextLine();
+                        if (dAmount <= 0) {
+                            System.out.println("금액은 0원보다 커야 합니다.");
+                            break;
+                        }
+                    } catch (InputMismatchException e) {
+                        System.out.println("금액은 숫자만 입력할 수 있습니다.");
+                        scanner.nextLine();
+                        break;
+                    }
                     myBank.deposit(dDate, dTitle, dAmount);
                     break;
                 case 2:
                     System.out.print("날짜(MMDD): ");
                     String wDate = formatDate(scanner.nextLine());
+                    if (!isValidDate(wDate)) {
+                        System.out.println("날짜 형식이 올바르지 않습니다.");
+                        break;
+                    }
+
                     System.out.print("항목: ");
                     String wTitle = scanner.nextLine();
                     System.out.print("금액: ");
-                    int wAmount = scanner.nextInt();
+                    int wAmount;
+                    try {
+                        wAmount = scanner.nextInt();
+                        scanner.nextLine();
+                        if (wAmount <= 0) {
+                            System.out.println("금액은 0원보다 커야 합니다.");
+                            break;
+                        }
+                    } catch (InputMismatchException e) {
+                        System.out.println("금액은 숫자만 입력할 수 있습니다.");
+                        scanner.nextLine();
+                        break;
+                    }
+
+                    System.out.print("카테고리(식비/교통비/쇼핑/문화 등): ");
+                    String wCategory = scanner.nextLine();
+
                     System.out.print("고정지출인가요? (true/false): ");
-                    boolean isFixed = scanner.nextBoolean();
-                    myBank.withdraw(wDate, wTitle, wAmount, isFixed);
+                    boolean isFixed;
+                    try {
+                        isFixed = scanner.nextBoolean();
+                        scanner.nextLine();
+                    } catch (InputMismatchException e) {
+                        System.out.println("true 또는 false만 입력할 수 있습니다.");
+                        scanner.nextLine();
+                        break;
+                    }
+                    myBank.withdraw(wDate, wTitle, wAmount, isFixed, wCategory);
                     break;
                 case 3:
                     myBank.showReport();
@@ -293,20 +373,46 @@ public class MyAccountManager {
                     break;
                 case 6:
                     myBank.showReport();
-                    System.out.println("수정 번호: ");
-                    int updateIdx = scanner.nextInt() - 1;
-                    scanner.nextLine();
+                    System.out.print("수정 번호: ");
+                    int updateIdx;
+                    try {
+                        updateIdx = scanner.nextInt() - 1;
+                        scanner.nextLine();
+                    } catch (InputMismatchException e) {
+                        System.out.println("번호는 숫자만 입력해 주세요.");
+                        scanner.nextLine();
+                        break;
+                    }
 
                     if (updateIdx >= 0 && updateIdx < myBank.getHistorySize()) {
                         System.out.print("새 날짜: ");
                         String nDate = formatDate(scanner.nextLine());
+                        if (!isValidDate(nDate)) {
+                            System.out.println("날짜 형식이 올바르지 않습니다.");
+                            break;
+                        }
                         System.out.print("새 항목: ");
                         String nTitle = scanner.nextLine();
                         System.out.print("새 금액: ");
-                        int nAmount = scanner.nextInt();
-                        scanner.nextLine();
+                        int nAmount;
+                        try {
+                            nAmount = scanner.nextInt();
+                            scanner.nextLine();
+                            if (nAmount <= 0) {
+                                System.out.println("금액은 0원보다 커야 합니다.");
+                                break;
+                            }
+                        } catch (InputMismatchException e) {
+                            System.out.println("금액은 숫자만 입력 가능합니다.");
+                            scanner.nextLine();
+                            break;
+                        }
 
-                        myBank.updateTransaction(updateIdx, nDate, nTitle, nAmount);
+                        System.out.print("새 카테고리: ");
+                        String nCategory = scanner.nextLine();
+
+                        myBank.updateTransaction(updateIdx, nDate, nTitle, nAmount, nCategory);
+                        System.out.println("수정이 완료되었습니다.");
                     } else {
                         System.out.println("해당 번호를 찾을 수 없습니다.");
                     }
@@ -314,8 +420,15 @@ public class MyAccountManager {
                 case 7:
                     myBank.showReport();
                     System.out.print("삭제 내역 번호: ");
-                    int deleteIdx = scanner.nextInt() - 1;
-                    scanner.nextLine();
+                    int deleteIdx;
+                    try {
+                        deleteIdx = scanner.nextInt() - 1;
+                        scanner.nextLine();
+                    } catch (InputMismatchException e) {
+                        System.out.println("번호는 숫자만 입력해주세요.");
+                        scanner.nextLine();
+                        break;
+                    }
 
                     if (deleteIdx >= 0 && deleteIdx < myBank.getHistorySize()) {
                         myBank.deleteTransaction(deleteIdx);
